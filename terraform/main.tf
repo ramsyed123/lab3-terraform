81472495ec010729c51c8089852f6f9469ec4941
ramsyed123 <rameez786cool@gmail.com>
2024-12-20 17:57:31 +0530

    terraform infra create

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,7 +82,7 @@ resource "aws_instance" "web1" {
   ami           = "ami-0e2c8caa4b6378d8c" 
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public1.id
-  security_groups = [aws_security_group.web_sg.name]
+  security_groups = [aws_security_group.web_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
